1465df73d5449df20788a03f7223c5c99ba1615a
zafar <zafarhussain633@gmail.com>
2022-08-24 23:41:47 +0530

    updated git ignore file

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # devcamp
-1. project setup need node 16 or higher
+1. project setup need node 16.15.1  or higher.
+   if node version abpve 16.15.1 did not work use 16.15.1 node version.
 2. add env with reference of env example
+3. npm install 
+4. npm run dev
+
 
 
--- server.js
+++ server.js
@@ -15,21 +15,18 @@ const app = express();
 //Body parser for req.body
 app.use(express.json())
 
-
-
 if(process.env.NODE_ENV ==="development"){ //it will only run in devlopement
     app.use(morgan("dev")) //from morgan third party loggger
 }
 
-app.use(cors());
-app.use(logger); //coustom logger                                                                       
-app.use(rootRouter);
-app.use(errorHadler);
+//middleware
+app.use(cors());         // it will allow all the originalUrl 
+app.use(logger);         //coustom logger                                                                       
+app.use(rootRouter);     // contain all routes
+app.use(errorHadler);    //for catching errors
 
-// app.use(asyncHandler)
 
 const PORT =  process.env.PORT || 5000;
-
 const server = app.listen(
     PORT,
     console.log("server running on port:"+ PORT)
